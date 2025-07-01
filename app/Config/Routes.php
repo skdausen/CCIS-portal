bba0cc35beed2005358bb2e6183160658b1c7098
skdausen <skdausen@gmail.com>
2025-07-01 08:15:35 +0800

    initial commit

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -8,10 +8,6 @@ use CodeIgniter\Router\RouteCollection;
 $routes->get('/', 'Home::index');
 
 use App\Controllers\Pages;
-use App\Controllers\AuthController;
 
 $routes->get('pages', [Pages::class, 'index']);
 $routes->get('(:segment)', [Pages::class, 'view']);
-
-$routes->get('/forgot-password', 'AuthController::forgotPassword');
-$routes->post('/send-reset-link', 'AuthController::sendResetLink');

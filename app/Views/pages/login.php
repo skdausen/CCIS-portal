b0200ab19cff9c5a078f1b8436749daa31b8ab93
Jose Gironella <gironellajose09@gmail.com>
2025-07-01 13:07:06 +0800

    login feature with mascot

--- app/Views/pages/login.php
+++ app/Views/pages/login.php
@@ -16,9 +16,9 @@
             <div class="col-md-6 d-flex align-items-center justify-content-center bg-white">
             <div class="card shadow p-4" style="width: 100%; max-width: 400px; border-radius: 15px;">
                 <img src="<?= base_url('rsc/assets/cs-logo.png'); ?>" alt="" class="mb-3 mx-auto login-img">
-                <h3 class="mb-4 text-center">Login Portal</h3>
-                <p class="text-center text-muted small mb-4">
-                Lorem ipsum dolor sit amet consectetur. Aliquam dictumst nisl imperdiet scelerisque ut urna.
+                <h3 class="mb-3 text-center">Login Portal</h3>
+                <p class="text-center text-muted small mb-5">
+                Enter your credentials to access your CCIS account.
                 </p>
 
                 <?php if (session()->getFlashdata('error')): ?>
@@ -54,8 +54,8 @@
             <div class="col-md-6 d-flex flex-column justify-content-center align-items-center bg-gradient" style="background: linear-gradient(to bottom right, #e0f7fa, #b2ebf2);">
             <div class="text-center px-4">
                 <h2 class="fw-bold">Welcome to AdaL</h2>
-                <p class="text-muted">Lorem ipsum dolor sit amet consectetur. Iaculis sapien et vitae sit molestie viverra turpis faucibus.</p>
-                <img src="<?= base_url('rsc/assets/robot-mascot.png'); ?>" alt="Mascot" class="img-fluid" style="max-width: 300px;">
+                <p class="text-muted mb-5">Unlock a smarter experience with AdaL, your dedicated intelligent partner for seamless productivity.</p>
+                <img src="<?= base_url('rsc/assets/mascot.png'); ?>" alt="Mascot" class="img-fluid" style="max-width: 300px;">
             </div>
             </div>
         </div>

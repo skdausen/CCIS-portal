a2cda4055f03323051b6c0c828e3fe9d5508f836
JKarganilla <jedrickkarganilla1111@gmail.com>
2025-07-21 10:38:54 +0800

    Update LOGO route

--- app/Views/templates/faculty/faculty_header.php
+++ app/Views/templates/faculty/faculty_header.php
@@ -23,7 +23,7 @@
 
     <nav class="navbar navbar-expand-lg navbar-dark sticky-top shadow-darker">
         <div class="container-fluid position-relative">
-            <a class="navbar-brand ms-3" href="#">CCIS</a>
+            <a class="navbar-brand ms-3" href="<?= site_url('admin/home') ?>">CCIS</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
